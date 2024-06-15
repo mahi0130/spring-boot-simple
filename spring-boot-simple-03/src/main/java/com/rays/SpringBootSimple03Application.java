50b19ee994949a4106da035cc4dbb8b21b62bdc1
mahi0130 <mahisinghkasera@gmail.com>
2024-06-15 11:39:29 +0530

    ok

---
+++ spring-boot-simple-03/src/main/java/com/rays/SpringBootSimple03Application.java
@@ -0,0 +1,29 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class SpringBootSimple03Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootSimple03Application.class, args);
+
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfig() {
+		WebMvcConfigurer w = new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+		return w;
+	}
+
+}
